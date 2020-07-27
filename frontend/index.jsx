a9d6ba8e895dfce2e5e48998112b986e512de551
Andrew Elmore <andrew.elmore93@gmail.com>
2020-07-27 18:29:14 -0400

    heroku push 2

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
+    debugger;
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<h1>Welcome to Couchcorn</h1>, root);
 });
